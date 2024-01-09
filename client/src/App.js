0c1a28bac0fe994d25ce584c251d3a3fa39eeb23
ayan-joshi <ayanjoshi182003@gmail.com>
2024-01-09 16:35:57 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import Criteria1 from './pages/criteria1';
 import Dashboard from './pages/dashboard';
 import AdminC1 from './pages/adminC1';
 import AdminDash from './pages/admin-dash';
+import axios from 'axios';
 
 function App() {
+  axios.post('https://naac-qzz01japa-ayan-joshi.vercel.app/')
   return (
     <div className="App">
       <BrowserRouter>
