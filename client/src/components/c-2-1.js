bf6c147fd00449f498b8363e2bcb4d6abb3a1ff5
ayan-joshi <ayanjoshi182003@gmail.com>
2024-02-05 20:06:27 +0530

    commit

--- client/src/components/c-2-1.js
+++ client/src/components/c-2-1.js
@@ -7,6 +7,30 @@ export const Criteria21 = () => {
 
     const department = localStorage.getItem('department');
     const academicYear = localStorage.getItem('academicYear');
+    const [syllabusRevisionCount, setSyllabusRevisionCount] = useState("");
+    const [file2_1_1_1, setFile2_1_1_1] = useState(null);
+    const [file2_1_1_2, setFile2_1_1_2] = useState(null);
+
+    const saveSection2_1_1_1 = async() => {
+        const formdata = new FormData();
+
+        const sectionData = {
+            department,
+            academicYear,
+            file2_1_1_1,
+        };
+
+        for (const key in sectionData) {
+            formdata.append(key, sectionData[key]);
+        }
+        try{
+            const response = await axios.post("http://localhost:5000/data/save2-1-1-1", formdata);
+            console.log(response.data); 
+            alert("Saved Section 2.1.1.1 data:");
+        }catch(error){
+            console.log("Error",error.message);
+        }
+    };
 
     const downloadExcel = async (exc_file) => {
         const templateFilePath = `${process.env.PUBLIC_URL}/${exc_file}`;
@@ -25,15 +49,17 @@ export const Criteria21 = () => {
   return (
    <div class='c-2-1'>
     <h3>2.1 - Student Enrollment and Profile</h3>
+    <br></br>
 
     <h4>2.1.1 - Demand Ratio</h4>
+    <br></br>
 
     <h4>2.1.1.1 - Number of seats available during the year</h4>
     <input
                             type="number"
-                            // id="syllabusRevisionCount"
-                            // value={syllabusRevisionCount}
-                            // onChange={(e) => setSyllabusRevisionCount(e.target.value)}
+                            id="syllabusRevisionCount"
+                            value={syllabusRevisionCount}
+                            onChange={(e) => setSyllabusRevisionCount(e.target.value)}
                         /><br />
      <table>
                             <thead>
@@ -58,13 +84,13 @@ export const Criteria21 = () => {
                                             id="file1_1_2_1"
                                             name="fileUpload"
                                             accept=".xls, .xlsx"
-                                            onChange={(e) => setFile1_1_2_1(e.target.files[0])}
+                                            onChange={(e) => setFile2_1_1_1(e.target.files[0])}
                                         />
                                     </td>
                                     <td>xls, xlsx. File size: 6MB</td>
                                 </tr>
                                 <tr>
-                                    <td> {file1_1_2_2 === 'true' ? (<span style={{ color: 'green', fontWeight: 'bold' }}>&#10003;</span>) : (<span style={{ color: 'red', fontWeight: 'bold' }}></span>)}
+                                    <td> {file2_1_1_2 === 'true' ? (<span style={{ color: 'green', fontWeight: 'bold' }}>&#10003;</span>) : (<span style={{ color: 'red', fontWeight: 'bold' }}></span>)}
                                         Upload relevant supporting documents</td>
                                     <td></td>
                                     <td>
@@ -73,17 +99,35 @@ export const Criteria21 = () => {
                                             id="file1_1_2_2"
                                             name="fileUpload"
                                             accept=".xls, .xlsx, .doc, .docx, .pdf"
-                                            onChange={(e) => setFile1_1_2_2(e.target.files[0])}
+                                            onChange={(e) => setFile2_1_1_2(e.target.files[0])}
                                         />
                                     </td>
                                     <td>xls, xlsx, doc, docx, pdf. <b>File size: 6MB</b></td>
                                 </tr>
                             </tbody>
                         </table>
+                        <div>
+                            <button onClick={saveSection2_1_1_1}>Save</button>
+                        </div> 
+                        <br></br>
+
+         <h3>2.1.2 - Total number of seats filled against reserved categories (SC, ST, OBC, Divyangjan, etc.) as per applicable reservation<br></br>policy during the year (Excluding Supernumerary Seats)</h3>
+         <br></br>
+
+         <h3>2.1.2.1 - Number of actual students admitted from the reserved categories during the year</h3>
+         <br></br>
+
+         <input
+                            type="number"
+                            id="syllabusRevisionCount"
+                            value={syllabusRevisionCount}
+                            onChange={(e) => setSyllabusRevisionCount(e.target.value)}
+                        /><br />
+
+        
     
    </div>
   )
 }
 
 export default Criteria21;
-
